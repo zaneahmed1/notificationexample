ee03fb862dabb663a5bd3cd0e64224eb152c1787
zaneahmed1 <zaneahmedd4@gmail.com>
2022-11-26 15:58:12 -0500

    changes again

--- event-scheduler-with-push-notifications/client/public/firebase-messaging-sw.js
+++ event-scheduler-with-push-notifications/client/public/firebase-messaging-sw.js
@@ -9,14 +9,14 @@ importScripts(
 // Initialize the Firebase app in the service worker by passing the generated config
 
 const firebaseConfig = {
-	apiKey: "...",
-	authDomain: "...",
-	projectId: "...",
-	storageBucket: "...",
-	messagingSenderId: "...",
-	appId: "...",
-	measurementId: "...",
-};
+	apiKey: "AIzaSyDRmSuzynOynHefdtj-QpcWaWgWcrHaUJI",
+	authDomain: "notificationexample-507cf.firebaseapp.com",
+	projectId: "notificationexample-507cf",
+	storageBucket: "notificationexample-507cf.appspot.com",
+	messagingSenderId: "719122766956",
+	appId: "1:719122766956:web:928d6098f2881cad084c49"
+  };
+  
 
 firebase.initializeApp(firebaseConfig);
 
